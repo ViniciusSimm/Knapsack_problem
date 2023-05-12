a7de4e32f39c8b9db79645effe2e39356eab5b74
Vinicius Sylvestre <vinicius.sylvestre@hypeone.com>
2023-05-12 16:05:38 -0300

    ready for the first batch

--- main.py
+++ main.py
@@ -4,9 +4,13 @@ from csv import writer
 from constructor import InitialConstructor
 from heuristic import Heuristic
 from model import Model
-from data import WEING1,SENTO1,SENTO2,WEING2,WEING3
+from data import WEING1,WEING2,WEING3,WEING4,WEING5,WEING6,WEING7,WEING8
+from data import WEISH01,WEISH02,WEISH03,WEISH04,WEISH05,WEISH06,WEISH07,WEISH08,WEISH09,WEISH10,WEISH11,WEISH12,WEISH13,WEISH14,WEISH15,WEISH16,WEISH17,WEISH18,WEISH19,WEISH20,WEISH21,WEISH22,WEISH23,WEISH24,WEISH25,WEISH26,WEISH27,WEISH28,WEISH29,WEISH30
+from data import SENTO1,SENTO2
+from data import PB1,PB2,PB4,PB5,PB6,PB7
+from data import HP1,HP2
 
-data = WEING3()
+data = WEING1()
 model = Model(weights=data.weights,rest=data.rest,capacities=data.capacities)
 constructor = InitialConstructor(weights=data.weights,rest=data.rest,capacities=data.capacities)
 heuristic = Heuristic()
@@ -48,9 +52,10 @@ for sol in solution_list:
 
 lista_ordenada = sorted(local_optimas, key=lambda x: x[1], reverse=True)
 print(lista_ordenada)
+print(data.optimum)
 
 with open('GRASP.csv', 'a') as f_object:
     writer_object = writer(f_object)
-    List = [data.name_data,N_SOLUTIONS,ALPHA,lista_ordenada[0][0],lista_ordenada[0][1]]
+    List = [data.name_data,N_SOLUTIONS,ALPHA,lista_ordenada[0][0],lista_ordenada[0][1],(data.optimum-lista_ordenada[0][1])/data.optimum]
     writer_object.writerow(List)
     f_object.close()
--- model.py
+++ model.py
@@ -21,22 +21,3 @@ class Model():
         sum = np.sum(mult)
         print(sum)
         return sum
-
-    
-
-
-# weights = [1898, 440, 22507, 270, 14148, 3100, 4650, 30800, 615, 4975, 
-#         1160, 4225, 510, 11880, 479, 440, 490, 330, 110, 560, 
-#         24355, 2885, 11748, 4550, 750, 3720, 1950, 10500]
-
-# weights = np.array(weights)
-
-# capacities = [600,600]
-# capacities = np.array(capacities)
-
-# rest1 = [45, 0, 85, 150, 65, 95, 30, 0, 170, 0, 40, 25, 20, 0, 0, 25, 0, 0, 25, 0, 165, 0, 85, 0, 0, 0, 0, 100]
-# rest2 = [30, 20, 125, 5, 80, 25, 35, 73, 12, 15, 15, 40, 5, 10, 10, 12, 10, 9, 0, 20, 60, 40, 50, 36, 49, 40, 19, 150]
-# rest = np.array([rest1,rest2])
-
-# creator = Creator(weights,rest,capacities)
-# creator.greedy_construction()
